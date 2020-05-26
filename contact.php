a6390752f4b65ee68f651724391f2084e9e50aaf
AbhisJk <65753288+AbhisJk@users.noreply.github.com>
2020-05-25 22:07:11 -0700

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,17 @@
+<?php
+	if(isset($_POST['submit'])){
+		$name=$_POST['name'];
+		$email=$_POST['email'];
+		$phone=$_POST['phone'];
+		$msg=$_POST['message'];
+
+		$to='crossroads@packapeer.com'; // Receiver Email ID, Replace with your email ID
+		$subject='Form Submission-Crossroads';
+		$message="Name :".$name."\n"."Phone :".$phone."\n"."Wrote the following :"."\n\n".$msg;
+		$headers="From: ".$email;
+
+		mail($to, $subject, $message, $headers);
+		
+	
+	}
+?>
